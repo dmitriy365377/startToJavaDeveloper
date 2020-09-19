5ac7b5cbec5f4bedd6b37968af4adc70219d26c7
dmitriy365377 <tigr365377@gmail.com>
2020-09-19 16:18:57 +0300

    binary search

--- Main.java
+++ Main.java
@@ -1,5 +1,72 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 5 5
+// 1 3 5 7 9 - N
+// 2 4 8 1 6 - K
+
+//1
+//3
+//7
+//1
+//5
+
+// k n
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JAVA СИЛА");
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt();
+        int K = s.nextInt();
+
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        for (int i = 0; i < K; i++) {
+            System.out.println(binarySearch(arr, s.nextInt()) != -1 ? "YES" : "NO");
+        }
+
+        // System.out.println(Arrays.toString(arr));
     }
+
+
+    //5 1
+    //1 3 5 7 9 
+    //2 
+
+    public static int binarySearch(int[] arr, int x) {
+        int left = 0; //0
+        int right = arr.length; //5 // 2
+      
+        while (left < right) {
+            int middle = (left + right) / 2; // 5/2 = 2  // 1
+                 // 5           // 2
+            if (arr[middle] == x) {
+                return middle;
+                         // 1 < 2
+            } else if (arr[middle] < x) {
+                left = middle + 1;
+                // 5 > 2
+            } else if (arr[middle] > x) {
+                        // 2
+                right = middle;
+            }
+
+        }
+
+        if(arr[left] > arr[left - 1]) {
+           System.out.println("closer " + arr[left - 1]);
+        } else {
+            System.out.println("closer " + arr[left]); 
+        }
+
+        System.out.println("left " + left);
+        return -1;
+    }
+
 }
+
---
+++ operators.java
@@ -0,0 +1,23 @@
+public class operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        a = a + 5;
+        a += 5;
+
+        a = a + 1;
+        a += 1;
+        a++;
+
+
+        // == != <> <= >= && || ?:
+
+        int x = 3;
+        int y = 5;
+        int z = 8;
+
+        boolean boolVal =  y < z;
+
+        System.out.println(boolVal);
+    }
+}
