b25f40b95ef36e006935e314b92f6a6f0111b82c
dmitriy365377 <tigr365377@gmail.com>
2020-10-03 10:47:06 +0300

    file

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+import jdk.internal.org.jline.terminal.impl.jna.win.Kernel32.FOCUS_EVENT_RECORD;
+
 // 5 5
 // 1 3 5 7 9 - N
 // 2 4 8 1 6 - K
@@ -26,9 +28,9 @@ public class Main {
             arr[i] = s.nextInt();
         }
 
-        for (int i = 0; i < K; i++) {
-            System.out.println(binarySearch(arr, s.nextInt()) != -1 ? "YES" : "NO");
-        }
+        // for (int i = 0; i < K; i++) {
+        //     System.out.println(binarySearch(arr, s.nextInt()));
+        // }
 
         // System.out.println(Arrays.toString(arr));
     }
@@ -37,36 +39,58 @@ public class Main {
     //5 1
     //1 3 5 7 9 
     //2 
+// https://www.ideserve.co.in/learn/img/merge-linked-lists-example.gif
+// https://www.codesdope.com/staticroot/images/algorithm/merge.gif
 
-    public static int binarySearch(int[] arr, int x) {
-        int left = 0; //0
-        int right = arr.length; //5 // 2
-      
-        while (left < right) {
-            int middle = (left + right) / 2; // 5/2 = 2  // 1
-                 // 5           // 2
-            if (arr[middle] == x) {
-                return middle;
-                         // 1 < 2
-            } else if (arr[middle] < x) {
-                left = middle + 1;
-                // 5 > 2
-            } else if (arr[middle] > x) {
-                        // 2
-                right = middle;
+    // public static int sort(int[] arr) {
+
+    // }
+
+    // https://informatics.mccme.ru/mod/statements/view3.php?id=33364&chapterid=111912#1
+        // [3,5,6]
+        // [1,2,3,7]
+    public static int[] merge(int[] array1, int[] array2) {
+        int newArray[] = new int[array1.length + array2.length];
+
+        int array1Index = 0;
+        int array2Index = 0;
+        
+        for(int i = 0; i < array1.length; i++) {
+            if(array1[array1Index] >= array2[array2Index]) {
+                
             }
-
+          
         }
-
-        if(arr[left] > arr[left - 1]) {
-           System.out.println("closer " + arr[left - 1]);
-        } else {
-            System.out.println("closer " + arr[left]); 
-        }
-
-        System.out.println("left " + left);
-        return -1;
+        
     }
 
 }
 
+
+
+// int left = 0; //0
+// int right = arr.length; //5 // 2
+
+// while (left < right) {
+//     int middle = (left + right) / 2; //1
+//          // 5           // 2
+//     if (arr[middle] == x) {
+//         return arr[middle];
+//                  // 1 < 2
+//     } else if (arr[middle] < x) {
+//         left = middle + 1;
+//         // 5 > 2
+//     } else if (arr[middle] > x) {
+//                 // 2
+//         right = middle;
+//     }
+
+// }
+
+// int b = arr[left-1] - x;
+// if(0 >= b) {
+//     return arr[left-1];
+// } else {
+//     return arr[left];
+// }
+// }
