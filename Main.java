662bbeb95647673eecd1e89b983e905017d4d8d6
Верховцов Дмитрий <verhovcovdmitrij@MacBook-Pro-Verhovcov.local>
2020-10-03 12:24:10 +0300

    concat two arrays

--- Main.java
+++ Main.java
@@ -1,96 +1,68 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-import jdk.internal.org.jline.terminal.impl.jna.win.Kernel32.FOCUS_EVENT_RECORD;
-
-// 5 5
-// 1 3 5 7 9 - N
-// 2 4 8 1 6 - K
-
-//1
-//3
-//7
-//1
-//5
-
 // k n
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
 
-        int N = s.nextInt();
-        int K = s.nextInt();
+ // public static void main(String[] args) {
+    //     Scanner s = new Scanner(System.in);
 
-        int arr[] = new int[N];
+    //     int N = s.nextInt();
+    //     int K = s.nextInt();
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = s.nextInt();
-        }
+    //     int arr[] = new int[N];
 
-        // for (int i = 0; i < K; i++) {
-        //     System.out.println(binarySearch(arr, s.nextInt()));
-        // }
+    //     for (int i = 0; i < N; i++) {
+    //         arr[i] = s.nextInt();
+    //     }
 
-        // System.out.println(Arrays.toString(arr));
-    }
+    //     for (int i = 0; i < K; i++) {
+    //     System.out.println(merge(arr, s.nextInt()));
+    //     }
 
-
-    //5 1
-    //1 3 5 7 9 
-    //2 
-// https://www.ideserve.co.in/learn/img/merge-linked-lists-example.gif
-// https://www.codesdope.com/staticroot/images/algorithm/merge.gif
-
-    // public static int sort(int[] arr) {
-
+    //     System.out.println(Arrays.toString(arr));
     // }
 
     // https://informatics.mccme.ru/mod/statements/view3.php?id=33364&chapterid=111912#1
-        // [3,5,6]
-        // [1,2,3,7]
-    public static int[] merge(int[] array1, int[] array2) {
-        int newArray[] = new int[array1.length + array2.length];
+    // [3,5,6]
+    // [1,2,3,7]
+    public class Main {
 
-        int array1Index = 0;
-        int array2Index = 0;
-        
-        for(int i = 0; i < array1.length; i++) {
-            if(array1[array1Index] >= array2[array2Index]) {
-                
+        public static int[] merge(int[] array1, int[] array2) {
+            int newArray[] = new int[array1.length + array2.length];
+
+            int array1Index = 0;
+            int array2Index = 0;
+
+            for (int k = 0; k < newArray.length; k++) {
+                if (k > array1.length - 1) {
+                    int a = array2[array2Index];
+                    newArray[k] = a;
+                    array2Index++;
+                } else if (array2Index > array2.length - 1) {
+                    int a = array1[array1Index];
+                    newArray[k] = a;
+                    array1Index++;
+                } else if (array1[array1Index] < array2[array2Index]) {
+                    int a = array1[array1Index];
+                    newArray[k] = a;
+                    array1Index++;
+                } else {
+                    int b = array2[array2Index];
+                    newArray[k] = b;
+                    array2Index++;
+                }
             }
-          
+            return newArray;
         }
-        
-    }
 
-}
+        public static void main(String[] args) {
+            int[] array1 = { 1, 2 };
+            int[] array2 = { 6, 7 };
+    
+            int[] rezult = merge(array1, array2);
+            System.out.println(Arrays.toString(rezult));
+        }
 
+    };
 
-
-// int left = 0; //0
-// int right = arr.length; //5 // 2
-
-// while (left < right) {
-//     int middle = (left + right) / 2; //1
-//          // 5           // 2
-//     if (arr[middle] == x) {
-//         return arr[middle];
-//                  // 1 < 2
-//     } else if (arr[middle] < x) {
-//         left = middle + 1;
-//         // 5 > 2
-//     } else if (arr[middle] > x) {
-//                 // 2
-//         right = middle;
-//     }
-
-// }
-
-// int b = arr[left-1] - x;
-// if(0 >= b) {
-//     return arr[left-1];
-// } else {
-//     return arr[left];
-// }
-// }
